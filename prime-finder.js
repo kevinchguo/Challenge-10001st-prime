37e4f7f3051288ad13164b6e308be08b46d5feb6
Kevin Guo <Kevin@Kevins-Air.home>
2019-09-18 19:26:05 -1000

    NOT DONE

--- prime-finder.js
+++ prime-finder.js
@@ -1,3 +1,11 @@
 module.exports = function(n) {
-  // do work here
+  // do work \
+  let counter = 1;
+  let primeNum = 2;
+  if (primeNum === 2) {
+    return primeNum;
+  }
+  primeNum++;
+  do {} while (counter < n);
+  return primeNum;
 };
